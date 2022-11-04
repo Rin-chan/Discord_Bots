b284875d087bc7ecef0533c37b42b8d4ee8b64c4
Rin <82919982+Rin-chan@users.noreply.github.com>
2022-11-04 11:02:53 +0800

    Remove exploring and fighting stats

--- MineRPG/database/User.py
+++ MineRPG/database/User.py
@@ -13,11 +13,9 @@ class User(Base):
     exp = Column(Integer, nullable=False, default=0)
     gold = Column(Integer, nullable=False, default=0)
     weapon = Column(Integer, ForeignKey("weapon.id"), default=1)
-    exploring = Column(Integer, nullable=False, default=1)
     digging = Column(Integer, nullable=False, default=1)
     mining = Column(Integer, nullable=False, default=1)
     farming = Column(Integer, nullable=False, default=1)
-    fighting = Column(Integer, nullable=False, default=1)
     enemy_id = Column(Integer, ForeignKey("enemy_fight.id"))
 
     weapon_relationship = relationship("Weapon", back_populates="user", foreign_keys=[weapon])
--- MineRPG/user/main.py
+++ MineRPG/user/main.py
@@ -17,11 +17,9 @@ async def get_details(ctx):
     message += f"Exp: {user_result.exp}\n"
     message += f"Gold: {user_result.gold}\n"
     message += f"Weapon: {user_result.weapon}\n"
-    message += f"Exploring: {user_result.exploring}\n"
     message += f"Digging: {user_result.digging}\n"
     message += f"Mining: {user_result.mining}\n"
     message += f"Farming: {user_result.farming}\n"
-    message += f"Fighting: {user_result.fighting}\n"
 
     message += "```"
     await ctx.send(message)
